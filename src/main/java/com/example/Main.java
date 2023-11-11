a44e86454f9cd93701ec59da8659a076cbc1ec3c
zy <2792965803@qq.com>
2023-11-11 14:53:56 +0800

    first

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * @author
+ * @version:1.0
+ * @description
+ * @create ${YEAR}-${MONTH}-${DAY}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
