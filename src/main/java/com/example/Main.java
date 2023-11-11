32386f6e20caf2b7b0b53809b28deb43540e23e4
zy <2792965803@qq.com>
2023-11-11 15:42:07 +0800

    hot-fix commit1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,6 +9,8 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!4");
         
     }
 }
